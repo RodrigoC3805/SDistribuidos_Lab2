aa933e8da8682a926862f12605027f22b0c80639
RodrigoC3805 <rodrigocastillo3805@gmail.com>
2026-05-06 21:44:23 -0500

    fixed urls

--- rpccliente.py
+++ rpccliente.py
@@ -1,4 +1,4 @@
 import xmlrpc.client
 
-proxy = xmlrpc.client.ServerProxy("INGRESAR_IP_DEL_SERVIDOR:12000/")
+proxy = xmlrpc.client.ServerProxy("http://INGRESAR_IP_DEL_SERVIDOR:12000/")
 print(proxy.procesar("HoLa MuNDo!"))
