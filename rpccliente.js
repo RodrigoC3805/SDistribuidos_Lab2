83cafa5f55869426a934035e804c07f05ec640a8
RodrigoC3805 <rodrigocastillo3805@gmail.com>
2026-05-09 18:27:58 -0500

    added js client

---
+++ rpccliente.js
@@ -0,0 +1,28 @@
+const xmlrpc = require("xmlrpc");
+const readline = require("readline");
+
+const client = xmlrpc.createClient({
+    host: "localhost",
+    port: 12000,
+    path: "/"
+});
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Ingrese una cadena: ", function(cadena) {
+
+    client.methodCall("procesar", [cadena], function(error, value) {
+
+        if (error) {
+            console.log("Error:", error);
+        } else {
+            console.log(value);
+        }
+
+        rl.close();
+    });
+
+});
--- rpccliente.py
+++
@@ -1,5 +0,0 @@
-import xmlrpc.client
-
-proxy = xmlrpc.client.ServerProxy("http://INGRESAR_IP_DEL_SERVIDOR:12000/")
-cadena = input("Ingrese una cadena: ")
-print(proxy.procesar(cadena))
